02cc50aa09e92c4ccedb35b554b399d95d4ad7c8
paura-mirez <paulina.rn@gmail.com>
2022-07-14 22:03:38 -0400

    Make function's playerSelection parameter case-insensitive

--- rps.js
+++ rps.js
@@ -10,3 +10,19 @@ function computerPlay () {
 
 }
 
+function playRound(playerSelection, computerSelection) {
+    if (playerSelection=="rock"|| playerSelection=="Rock"|| playerSelection=="ROck"|| playerSelection=="ROCk"||playerSelection=="ROCK"||playerSelection=="RocK"|| playerSelection=="rocK"|| playerSelection=="roCK") {
+        playerSelection="Rock";
+    }
+    if (playerSelection=="scissors"|| playerSelection=="Scissors"|| playerSelection=="SCissors"|| playerSelection=="SCIssors"|| playerSelection=="SCISsors"|| playerSelection=="SCISSORS") {
+        playerSelection="Scissors";
+    }
+    if (playerSelection=="paper"|| playerSelection=="Paper"|| playerSelection=="PAPer"|| playerSelection=="PAper"|| playerSelection=="PAPEr"|| playerSelection=="PAPER"){
+        playerSelection="Paper";
+    }
+    console.log(playerSelection)
+  }
+   
+  const playerSelection = "rock";
+  const computerSelection = computerPlay();
+  console.log(playRound(playerSelection, computerSelection));
