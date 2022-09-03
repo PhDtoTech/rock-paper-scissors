7a857098a3319c12c76b0cebdc67f0e76ebb9de9
Paulina Ramirez <paulina.rn@gmail.com>
2022-09-02 19:45:51 -0500

    Add User Interface that keeps score and shows user and computer selections

--- rps.js
+++ rps.js
@@ -1,16 +1,6 @@
-//This is the function that allows the computer to randomly choose an option
-  function computerPlay () {
-    function getRandomInt() {
-        return Math.floor(Math.random() * 3);
-      }
-    const rps=["Rock", "Paper", "Scissors"];
-    return rps[getRandomInt()];
-}
 
-let playScore = 0;
-let compScore= 0;
 
-function game () {
+/*function game () {
     
     for (let i = 0; i < 5; ) {
         
@@ -35,11 +25,11 @@ function game () {
         }
 
      }
-  }
+  }*/
 
 
 //This is the function to play a round of the game
-function playRound(playerSelection, computerSelection) {
+/*function playRound(playerSelection, computerSelection) {
 
     if (playerSelection=="rock"|| playerSelection=="Rock"|| playerSelection=="ROck"|| playerSelection=="ROCk"||playerSelection=="ROCK"||playerSelection=="RocK"|| playerSelection=="rocK"|| playerSelection=="roCK") {
         playerSelection="Rock";
@@ -69,4 +59,136 @@ function playRound(playerSelection, computerSelection) {
         return "Lose"
   }
     else { return "There seems to be a problem. Try again."}
+}*/
+
+//This is the function to play a round of the game
+
+const playerScore = document.querySelector('#playerScore');
+
+const computerScore = document.querySelector('#computerScore');
+
+window.onload = (event) => {
+    playerScore.innerHTML = playScore;
+    computerScore.textContent = compScore;
+  };
+
+
+//This is the function that allows the computer to randomly choose an option
+function computerPlay () {
+    function getRandomInt() {
+        return Math.floor(Math.random() * 3);
+      }
+    const rps=["Rock", "Paper", "Scissors"];
+    return rps[getRandomInt()];
+    }
+
+let playScore = 0;
+let compScore = 0;
+
+let playerSelection;
+
+let computerSelection = computerPlay();
+
+const rock = document.querySelector('#rock');
+
+const paper = document.querySelector('#paper');
+
+const scissors = document.querySelector('#scissors');
+
+const playerChoice = document.querySelector('#playerChoice');
+
+const computerChoice = document.querySelector('#computerChoice');
+
+function playRound(playerSelection, computerSelection) {
+
+    if (playScore<5 && compScore<5){
+        if ((playerSelection==="Rock" && computerSelection==="Scissors") ||
+        (playerSelection==="Scissors" && computerSelection==="Paper") ||
+        (playerSelection==="Paper" && computerSelection==="Rock") ) {
+            playScore++;
+        }
+    else if ((playerSelection==="Scissors" && computerSelection==="Rock") ||
+            (playerSelection==="Paper" && computerSelection==="Scissors") ||
+            (playerSelection==="Rock" && computerSelection==="Paper") ) {
+                compScore++;
+            }
+    }
+    else if (playScore==5 || compScore==5){
+
+        playScore=0;
+        compScore=0;
+
+        if ((playerSelection==="Rock" && computerSelection==="Scissors") ||
+            (playerSelection==="Scissors" && computerSelection==="Paper") ||
+            (playerSelection==="Paper" && computerSelection==="Rock") ) {
+                playScore++;
+        }
+        else if ((playerSelection==="Scissors" && computerSelection==="Rock") ||
+                (playerSelection==="Paper" && computerSelection==="Scissors") ||
+                (playerSelection==="Rock" && computerSelection==="Paper") ) {
+                    compScore++;
+            }
+    }
+    
+
 }
+
+rock.addEventListener('click', () => {
+    playerSelection = "Rock";
+    computerSelection = computerPlay();
+    playRound(playerSelection, computerSelection);
+    playerChoice.textContent = playerSelection;
+    computerChoice.textContent = computerSelection;
+    playerScore.textContent = playScore;
+    computerScore.textContent = compScore;
+    if (playScore==5) {
+        alert ("You won!");
+    }
+    else if (compScore==5){
+        alert ("The computer won!");
+    }
+});
+
+paper.addEventListener('click', () => {
+    playerSelection = "Paper";
+    computerSelection = computerPlay();
+    playRound(playerSelection, computerSelection);
+    playerChoice.textContent = playerSelection;
+    computerChoice.textContent = computerSelection;
+    playerScore.textContent = playScore;
+    computerScore.textContent = compScore;
+    if (playScore==5) {
+        alert ("You won!");
+    }
+    else if (compScore==5){
+        alert ("The computer won!");
+    }
+    
+});
+
+scissors.addEventListener('click', () => {
+    playerSelection = "Scissors";
+    computerSelection = computerPlay();
+    playRound(playerSelection, computerSelection);
+    playerChoice.textContent = playerSelection;
+    computerChoice.textContent = computerSelection;
+    playerScore.textContent = playScore;
+    computerScore.textContent = compScore;
+    if (playScore==5) {
+        alert ("You won!");
+    }
+    else if (compScore==5){
+        alert ("The computer won!");
+    }
+  
+});
+
+
+
+
+
+
+
+
+playerScore.textContent = playScore;
+computerScore.textContent = compScore;
